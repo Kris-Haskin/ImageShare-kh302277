823620d4c9ba06907023880cf5e04e9ec725e9fa
Kris-Haskin <kh302277@southeast.edu>
2022-09-03 17:41:11 -0500

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; import { Image, StyleSheet, TouchableOpacity, Text, View, Platform } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import * as ImageManipulator from "expo-image-manipulator";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 20);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [selectedImage, setsSelectedImage] = React.useState(null);
+
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera is required!")
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+
+        setsSelectedImage({ localUri: pickerResult.uri });
+
+        console.log(pickerResult);
+    };
+
+    let openShareDialogAsync = async () => {
+        if (Platform.OS === 'web') {
+            alert(`uh oh, sharing isn't available on your platform`);
+            return;
+        }
+
+        const imageTmp = await ImageManipulator.manipulateAsync(selectedImage.localUri);
+        await Sharing.shareAsync(imageTmp.uri);
+    };
+
+    if (selectedImage !== null) {
+        return (
+            <View style={styles.container}>
+                <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+                <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+                    <Text style={styles.buttonText}>Share this Photo</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+
+            <Text style={styles.instructions}>
+                To share a photo from your phone with a friend, just press the button below!
+            </Text>
+
+            <TouchableOpacity
+                onPress={openImagePickerAsync}
+                style={styles.button}>
+                <Text style={styles.buttonText}>Pick a Photo</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +76,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    },
+    logo: {
+        width: 305,
+        height: 159,
+        marginBottom: 10,
+    },
+    instructions: {
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+    },
+    button: {
+        backgroundColor: "blue",
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: "contain"
+    }
 });
